e2e78f9ec42385b304b3c6edb0abb1f46ee46807
Aashan M <aashan.meiyappan@uwaterloo.ca>
2020-12-30 16:44:47 -0500

    Duplicate Emails

---
+++ Leetcode/duplicate_emails.sql
@@ -0,0 +1,27 @@
+Write a SQL query to find all duplicate emails in a table named Person.
+
++----+---------+
+| Id | Email   |
++----+---------+
+| 1  | a@b.com |
+| 2  | c@d.com |
+| 3  | a@b.com |
++----+---------+
+For example, your query should return the following for the above table:
+
++---------+
+| Email   |
++---------+
+| a@b.com |
++---------+
+
+--------------------------------------------------------------------------------------
+
+SELECT Email
+FROM Person
+
+# group results by email
+GROUP BY Email
+
+# duplicate values would have count greater than 1 so filter by that
+HAVING COUNT(*) > 1
