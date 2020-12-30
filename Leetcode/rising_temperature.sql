03cf002d33662231d32441fb85ed8a3d5c3a9b93
Aashan M <aashan.meiyappan@uwaterloo.ca>
2020-12-30 17:26:17 -0500

    Rising Temperature

---
+++ Leetcode/rising_temperature.sql
@@ -0,0 +1,49 @@
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the primary key for this table.
+This table contains information about the temperature in a certain day.
+ 
+
+Write an SQL query to find all dates' id with higher temperature compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The query result format is in the following example:
+
+Weather
++----+------------+-------------+
+| id | recordDate | Temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+Result table:
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+In 2015-01-02, temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, temperature was higher than the previous day (20 -> 30).
+
+----------------------------------------------------------------------------------------------
+
+SELECT t1.id
+# select twice to compare values
+FROM Weather t1,
+     Weather t2
+# check the difference in days is 1
+WHERE DATEDIFF(t1.recordDate, t2.recordDate) = 1
+# make sure new temperature is higher than the old temperature
+AND t1.Temperature > t2.Temperature
